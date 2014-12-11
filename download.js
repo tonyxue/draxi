7747f844d5b0db17d17049db56b9d53bd8d397c3
Tony Xue <xuezxbb@gmail.com>
2014-12-11 20:34:17 +0800

    Added origin for download.js

--- download.js
+++ download.js
@@ -1,3 +1,7 @@
+// The original download.js was downloaded from 
+// http://www.storminthecastle.com/projects/scrape/scrape.zip
+// and the uncessary parts for this project are removed.
+
 var http = require("http");
 function download(url, callback) {
   http.get(url, function(res) {
